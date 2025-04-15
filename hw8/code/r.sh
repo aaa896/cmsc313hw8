567941a6bd6332b59e8e0abba4e1db42dc296dd6
aaa896 <reebbahshabbeer@gmail.com>
2025-04-15 18:55:34 -0400

    Update r.sh

--- hw8/code/r.sh
+++ hw8/code/r.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-g++ c_matrix.c -o c_mat && ./c_mat
-g++ cpp_matrix.cpp -o cpp_mat && ./cpp_mat
+gcc c_matrix.c -o c_mat && ./c_mat
+gcc cpp_matrix.cpp -o cpp_mat && ./cpp_mat
 read -p "..."
